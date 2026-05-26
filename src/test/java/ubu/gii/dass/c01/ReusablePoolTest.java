04f80acf561d6ce7dc8f2c445a290cf56ba4a8b2
zcc1001 <94805927+zcc1001@users.noreply.github.com>
2026-05-26 14:12:00 +0200

    Implementacion de test parra testGetInstance()

--- src/test/java/ubu/gii/dass/c01/ReusablePoolTest.java
+++ src/test/java/ubu/gii/dass/c01/ReusablePoolTest.java
@@ -3,28 +3,25 @@
  */
 package ubu.gii.dass.c01;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Disabled;
-
-
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 /**
- * @author alumno
+ * @author Zeldan Campos
  *
  */
 public class ReusablePoolTest {
 
-	
 	@BeforeAll
-	public static void setUp(){
+	public static void setUp() {
 	}
 
-	
 	@AfterAll
 	public static void tearDown() throws Exception {
 	}
@@ -32,32 +29,36 @@ public class ReusablePoolTest {
 	/**
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#getInstance()}.
 	 */
-        @Test
-        @DisplayName("testGetInstance")
-        @Disabled("Not implemented yet")
+	@Test
+	@DisplayName("testGetInstance")
 	public void testGetInstance() {
-		
+		ReusablePool i1 = ReusablePool.getInstance();
+		ReusablePool i2 = ReusablePool.getInstance();
+		assertNotNull(i1, "La instancia 1 no debe ser nula.");
+		assertNotNull(i2, "La instancia 2 no debe ser nula.");
+		assertSame(i1, i2, "Las dos instancias deben ser el mismo objeto (Singleton)");
 	}
 
 	/**
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#acquireReusable()}.
 	 */
 	@Test
-        @DisplayName("testAcquireReusable")
-        @Disabled("Not implemented yet")
+	@DisplayName("testAcquireReusable")
+	@Disabled("Not implemented yet")
 
 	public void testAcquireReusable() {
-		
+
 	}
 
 	/**
-	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
+	 * Test method for
+	 * {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
 	 */
 	@Test
-        @DisplayName("testReleaseReusable")
-        @Disabled("Not implemented yet")
+	@DisplayName("testReleaseReusable")
+	@Disabled("Not implemented yet")
 	public void testReleaseReusable() {
-		
+
 	}
 
 }
